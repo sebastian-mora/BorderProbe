0322a52f0e7a03685a898f2b170cbbbbe3668502
smora01 <Trashwang@707>
2019-06-14 13:23:58 -0700

    reformatting

--- helpers/Report.py
+++ helpers/Report.py
@@ -9,28 +9,39 @@ class Report:
     def __init__(self, xml_file_names, subnets, attacker_ip):
 
         """
+        Compiles all saved XML file into a HTML report
 
-        :param xml_file_names: A list of saved XML files from Scan #2
+        :param xml_file_names: A list of saved XML files
         :param subnets: A list of subnets that were scanned
         :param attacker_ip: The ip of where the scan originated
         """
         self.attacker_ip = attacker_ip
         self.scan_data = self.openXMLFiles(xml_file_names)
-        self.host_table_template = self.getBS('output/template/host_table.html')
-        self.subnet_table = self.getBS('output/template/subnet_table.html')
-        self.report = self.getBS('output/template/Final_Report.html')
+        self.host_table_template = self.getBS('helpers/templates/host_table.html')
+        self.subnet_table = self.getBS('helpers/templates/subnet_table.html')
+        self.report = self.getBS('helpers/templates/Final_Report.html')
         self.save_path = xml_file_names[0].split('/')[xml_file_names[0].index("output") + 1]
         self.subnets = subnets
         self.generateReport(self.save_path)
 
     def getBS(self, filename):
+        """
+        Opens HTML file and returns a BeautifulSoup object
+        :param filename: path for xml
+        :return: BeautifulSoup
+        """
         with open(filename)as f:
             html = BeautifulSoup(f, 'html.parser')
         return html
 
     def openXMLFiles(self, filenames):
-        xml_docs = []
+        """
+        Opens all XML files and saves them as dics in list
 
+        :param filenames: list of XMl file paths
+        :return: list [ dic ]
+        """
+        xml_docs = []
         for filename in filenames:
             with open(filename) as fd:
                 xml_doc = xmltodict.parse(fd.read())
@@ -41,16 +52,93 @@ class Report:
 
     def generateReport(self, file_path):
 
-        #  Appends subnets to Final Report
+        """
+        Generates main HTML report
+
+        :param file_path: path to save report
+        :return: None
+        """
+
+        #  Adds subnets to CIDR ranges
         self.report.find(id='cidr_ranges').string = ' '.join([subnet.compressed for subnet in self.subnets])
 
         report_num = 0
         for subnet in self.subnets:
+            # Generates subnet table
             subnet_table = self.generateSubnetTable(subnet, report_num)
             report_num += 1
             self.report.find(id='subnet_reports').append(subnet_table)
 
         self.saveReport(file_path)
+
+    def generateScreenShotTable(self, host):
+
+        """
+        Generates tables for all found hosts on subnets
+        :param host: host xml data
+        :return: HTML
+        """
+
+        table = copy.copy(self.host_table_template)
+
+        target_ip = host["address"]["@addr"]
+        open_ports = self.getOpenPorts(host)
+        os_detected = self.getTopOS(host)
+
+        table.find(class_='host_ip').string = target_ip
+
+        for port in open_ports:
+            li_new_tag = table.new_tag('li')
+            li_new_tag.string = port
+            table.find(class_='open_ports').append(li_new_tag)
+
+        count = 0
+        for os in os_detected:
+            if count == 3:
+                break
+            li_new_tag = table.new_tag('li')
+            li_new_tag.string = os
+            table.find(class_='os_detection').append(li_new_tag)
+            count += 1
+
+        return table
+
+    def generateSubnetTable(self, subnet, report_num):
+
+        #  TODO find better way to index scan data
+
+        """
+        Generates Tables for each subent
+        :param subnet:
+        :param report_num: used to index scan data
+        :return:
+        """
+
+        subnet_table = copy.copy(self.subnet_table)
+
+        for ip in subnet_table.findAll('span', class_='target'):
+            ip.string = subnet.compressed
+
+        subnet_table.find(class_='attacker').string = self.attacker_ip
+
+        #  For all found hosts generate a table for them and insert them into "ScreenShots"
+        try:
+            hosts = self.scan_data[report_num]['nmaprun']["host"]
+
+            if isinstance(hosts, dict):
+                table = self.generateScreenShotTable(hosts)
+                subnet_table.find(class_="hosts").append(table)
+            else:
+                for host in hosts:
+                    if host["status"]["@state"] == "up":
+                        table = self.generateScreenShotTable(host)
+                        subnet_table.find(class_="hosts").append(table)
+
+            return subnet_table
+
+        except:
+            print("No Hosts in this file")
+            return None
 
     def saveReport(self, output_folder):
         path = "output/%s/Final_Report.html" % output_folder
@@ -80,56 +168,3 @@ class Report:
 
         except:
             return ["No OS detected"]
-
-    def generateScreenShotTable(self, host):
-
-        table = copy.copy(self.host_table_template)
-
-        target_ip = host["address"]["@addr"]
-        open_ports = self.getOpenPorts(host)
-        os_detected = self.getTopOS(host)
-
-        table.find(class_='host_ip').string = target_ip
-
-        for port in open_ports:
-            li_new_tag = table.new_tag('li')
-            li_new_tag.string = port
-            table.find(class_='open_ports').append(li_new_tag)
-
-        count = 0
-        for os in os_detected:
-            if count == 3:
-                break
-            li_new_tag = table.new_tag('li')
-            li_new_tag.string = os
-            table.find(class_='os_detection').append(li_new_tag)
-            count += 1
-
-        return table
-
-    def generateSubnetTable(self, subnet, report_num):
-
-        subnet_table = copy.copy(self.subnet_table)
-
-        for ip in subnet_table.findAll('span', class_='target'):
-            ip.string = subnet.compressed
-
-        subnet_table.find(class_='attacker').string = self.attacker_ip
-
-        try:
-            hosts = self.scan_data[report_num]['nmaprun']["host"]
-
-            if isinstance(hosts, dict):
-                table = self.generateScreenShotTable(hosts)
-                subnet_table.find(class_="hosts").append(table)
-            else:
-                for host in hosts:
-                    if host["status"]["@state"] == "up":
-                        table = self.generateScreenShotTable(host)
-                        subnet_table.find(class_="hosts").append(table)
-
-            return subnet_table
-
-        except:
-            print("No Hosts in this file")
-            return None
--- helpers/Scanner.py
+++ helpers/Scanner.py
@@ -42,7 +42,6 @@ class Scanner:
         :return: list[IPv4sNetworks]
         """
         prefix_len = ipv4_subnet.prefixlen
-        print(prefix_len)
 
         if prefix_len < 24:
             prefix_len = 24 - prefix_len
@@ -61,8 +60,7 @@ class Scanner:
     def getTiming(self):
         menus.timingOptions()
         num = input("Pleas enter a Number: ")
-        timing = ['-T']
-        timing.append(str(num))
+        timing = ['-T', str(num)]
         return timing
 
     def getSpoofIP(self):
@@ -71,6 +69,16 @@ class Scanner:
         return flags
 
     def saveLiveHosts(self, live_hosts, subnet, foldername):
+
+        """
+        Saves live hosts to a CSV
+        CSV format: ip,subnet
+
+        :param live_hosts: list of found hosts
+        :param subnet: the subnet the hosts were found on
+        :param foldername: to where the csv will be saved
+        :return:
+        """
 
         if os.path.isdir('output/%s' % foldername) is not True:
             os.mkdir('output/%s' % foldername)
@@ -81,15 +89,14 @@ class Scanner:
         for host in live_hosts:
             f.write("%s,%s\n" % (str(host), subnet))
         f.close()
-        return filename
+
 
     def getSpoofMac(self):
 
         """
         Gens random Unicast Mac
-        https://stackoverflow.com/questions/8484877/mac-address-generator-in-python
 
-        :return:
+        :return: MAC Address str
         """
         mac = "02:00:00:%02x:%02x:%02x" % (random.randint(0, 255),
                                            random.randint(0, 255),
@@ -105,12 +112,13 @@ class Scanner:
         :param xml_string:
         :return: list(ipv4)
         """
-
+        #  Turns xml into dic
         data = xmltodict.parse(host_scan_results)
 
         try:
             hosts = data['nmaprun']["host"]
 
+            #  If only one host found result will be in dic not list
             if isinstance(hosts, dict):
                 ip = hosts['address']['@addr']
                 return [ip]
@@ -148,20 +156,18 @@ class Scanner:
                 else:
                     flag_list.append(self.evasionOptions[item])
             else:
-                print("invaild choice: %s", item)
+                print("Invalid choice: %s", item)
 
         self.evasion_used = flag_list
 
         return flag_list
 
     def hostScan(self, subnets, scan_selector):
-        # nmap -sn subnet
-
         """
-        Uses ping only scan and saves result to txt file
+        Discovers lives host
 
-        :param subnets: Full Subnet range
-        :param scan_selector: Selects type of nmap scan
+        :param subnets: list of all subnets to be scanned
+        :param scan_selector: user chosen scan type
         :return: Dic {subnet: [found_host ip]}
         """
 
@@ -171,15 +177,12 @@ class Scanner:
             3: ['-sn', '-PS22-25,80,3389', '-PA22-25,80,3389', '-iL', '-']  # Custom Scan
         }
 
-
-
         flags = scan_type[scan_selector]
 
         hosts = {}
 
         if scan_selector == 3:
             flags.extend(self.evasionTechniques())
-
 
         found_hosts = []
         folder_name = datetime.datetime.now().strftime('%X')
@@ -188,8 +191,11 @@ class Scanner:
         for subnet in subnets:
             subnet_div = self.divideSubnet(subnet)
 
+            #  Shuffles the order in which the subnets are scanned
             for random_subnet in self.randomizeSubnetOrder(subnet_div):
                 result = self.executeNmapCommand(flags, random_subnet.compressed)
+
+                #  extract live hosts from xml data
                 result = self.getLiveHosts(result)
 
                 if result:
@@ -205,19 +211,20 @@ class Scanner:
     def phaseTwoScan(self, host_dic):
 
         """
-            Using the list of live host from the first scan
-            This method will do a deeper scan suing the
-            flags (--randomize-hosts -n -Pn -A -sSVC (Phase 1 Evasion) --top-ports 1000 -iL filename-of-live-hosts.txt )
+            Using the dic of live host from the first scan
+            This method will do a deeper scan using the
+            flags (--randomize-hosts -n -Pn -A -sSVC (Phase 1 Evasion) --top-ports 1000 -iL -)
             *Any evasion Methods used in Phase one will also be applied
 
         :param host_dic: {subnet: [found_host ip]}
-        :return: [xml_file, .. ]
+        :return: [xml_file_name, ... ]
         """
         # flags = ['--randomize-hosts', '-n', '-Pn', '-O', '-sV', '--top-ports', '1000',
         #           '--script-timeout', '20', '-iL', live_hosts_file]
 
         flags = ['--randomize-hosts', '-n', '-Pn', '--top-ports', '100', '--script-timeout', '20', '-iL', '-']
         saved_files = []
+
         if self.evasion_used:
             flags.extend(self.evasion_used)
 
@@ -231,16 +238,15 @@ class Scanner:
 
         return saved_files
 
-    # TODO Make method read from host file and start process for subnets
-
-
-
-    def executeNmapCommand(self, flags, host_ips=None , file_name=None):
+    def executeNmapCommand(self, flags, host_ips, file_name=None):
 
         """
-        Execute an Nmap command.
-        :param file_name: If file name is set method will save Nmap Result. If not result will be returned from stdout
-        :param flags: ex. ' -B -f -P22'
+            Takes flags and ip list and pipe it into Nmap. The Nmap result is returned in XMl format
+            If a file name is specified it will save to it otherwise results will be returned on stdout
+
+        :param flags: nmap flags
+        :param host_ips: list of ips to be scanned
+        :param file_name: (opt) where to save results
         :return: stdout
         """
         flags = flags.copy()
@@ -261,12 +267,11 @@ class Scanner:
 
         stdout, stderr = p.communicate(input=host_ips.encode())
 
-
         menus.processAnimation(p)
 
-        print("Scan Complete for: %s" % flags[len(flags) - 1])
+        print("Scan Complete for: %s" % host_ips)
 
-
+        #  If nmap throws error print error and exit
         if "QUITTING" in str(stderr):
             print('\n' + str(stderr))
 
